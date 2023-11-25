ec72f0e800cf2a589d7f2f96876e202a32f196bb
Javier Piqueras <javierpiquerasmartinez@gmail.com>
2023-11-25 18:18:54 +0100

    Patient Card WIP

--- dentalcare-suite/src/App.jsx
+++ dentalcare-suite/src/App.jsx
@@ -4,8 +4,10 @@ import { PatientCard } from './components/PatientCard'
 export function App(){
     return (
         <>
-            <PatientCard firstName="Javier" familyName="Piqueras Martinez" phoneNumber="34655601562" address="javipiquerasm@gmail.com" details="Primer Paciente"/>
-            <PatientCard firstName="Kenza" familyName="El Alaoui" phoneNumber="34655601562" address="javipiquerasm@gmail.com" details="Primer Paciente"/>
+            <section className='dcs-patientsection'>
+                <PatientCard firstName="Javier" familyName="Piqueras Martinez" phoneNumber="+34655601562" address="javipiquerasm@gmail.com" details="Primer Paciente"/>
+                <PatientCard firstName="Kenza" familyName="El Alaoui" phoneNumber="+34655601562" address="javipiquerasm@gmail.com" details="Primer Paciente"/>
+            </section>
         </>
         )
 }
--- dentalcare-suite/src/components/PatientCard.jsx
+++ dentalcare-suite/src/components/PatientCard.jsx
@@ -2,13 +2,13 @@ import './PatientCard.css'
 
 export function PatientCard ({firstName, familyName, phoneNumber, address, details}) {
     return (
-        <article>
-        <header>{firstName} {familyName}</header>
-        <section>
-            <div>{phoneNumber}</div>
-            <div>{address}</div>
-            <div>{details}</div>
-        </section>
-    </article>
+        <article className='dcs-patientcard'>
+            <header>{firstName} {familyName}</header>
+            <section>
+                <div>{phoneNumber}</div>
+                <div>{address}</div>
+                <div>{details}</div>
+            </section>
+        </article>
     )
 }
