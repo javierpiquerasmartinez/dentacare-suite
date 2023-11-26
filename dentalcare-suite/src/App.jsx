e5fe654d31142e0865e211d5dafe7c7c118406b9
Javier Piqueras <javierpiquerasmartinez@gmail.com>
2023-11-26 20:45:50 +0100

    Basic View Patients

--- dentalcare-suite/src/App.jsx
+++ dentalcare-suite/src/App.jsx
@@ -10,7 +10,7 @@ export function App(){
             <main className='dcs-main'>
                 <section className='dcs-patientsection'>
                     <PatientCard firstName="Javier" familyName="Piqueras Martinez" phoneNumber="+34655601562" address="javipiquerasm@gmail.com" details="Primer Paciente"/>
-                    <PatientCard firstName="Kenza" familyName="El Alaoui" phoneNumber="+34655601562" address="javipiquerasm@gmail.com" details="Primer Paciente"/>
+                    <PatientCard firstName="Kenza" familyName="El Alaoui" phoneNumber="+34655601562" address="kea@gmail.com" details="Myself"/>
                 </section>
             </main>
         </>
--- dentalcare-suite/src/components/PatientCard.jsx
+++ dentalcare-suite/src/components/PatientCard.jsx
@@ -3,7 +3,11 @@ import './PatientCard.css'
 export function PatientCard ({firstName, familyName, phoneNumber, address, details}) {
     return (
         <article className='dcs-patientcard'>
-            <header>{firstName} {familyName}</header>
+            <header>
+                <h3>
+                {firstName} {familyName}
+                </h3>
+            </header>
             <section>
                 <div>{phoneNumber}</div>
                 <div>{address}</div>
