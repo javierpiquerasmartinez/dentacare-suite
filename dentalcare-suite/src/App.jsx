7bfda57be04e92090f682d9fa5a0b95914a7c390
Javier Piqueras <javierpiquerasmartinez@gmail.com>
2023-11-26 20:55:04 +0100

    changes in Css

--- dentalcare-suite/src/App.jsx
+++ dentalcare-suite/src/App.jsx
@@ -9,7 +9,7 @@ export function App(){
             </header>
             <main className='dcs-main'>
                 <section className='dcs-patientsection'>
-                    <PatientCard firstName="Javier" familyName="Piqueras Martinez" phoneNumber="+34655601562" address="javipiquerasm@gmail.com" details="Primer Paciente"/>
+                    <PatientCard firstName="Francisco Javier" familyName="Piqueras Martinez" phoneNumber="+34655601562" address="javipiquerasm@gmail.com" details="Primer Paciente"/>
                     <PatientCard firstName="Kenza" familyName="El Alaoui" phoneNumber="+34655601562" address="kea@gmail.com" details="Myself"/>
                 </section>
             </main>
